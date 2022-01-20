51daa8686ea516ba29d59041ad9fa874bf65174b
Arnavi Gode <86406228+arnavigode@users.noreply.github.com>
2022-01-21 00:39:52 +0530

    pop-up ui done

--- koo_app_clone/src/Components/LandingPage/LoginPopUp.jsx
+++ koo_app_clone/src/Components/LandingPage/LoginPopUp.jsx
@@ -3,10 +3,10 @@ import "./LoginPopup.css";
 
 const LoginPopUp = (props) => {
   return props.trigger ? (
-    <div className="popup-login">
+    <div id="unblurred" className="popup-login">
       <div className="popup-inner">
         <div className="popup-title">
-          <span>Enter your phone number</span>
+          <span style={{marginLeft:"2%"}}>Enter your phone number</span>
           <button className="close-btn">
             <img
               src="https://www.kooapp.com/assets/15c66d2b.svg"
@@ -37,17 +37,20 @@ const LoginPopUp = (props) => {
           ></input>
         </div>
         <div className="email-verify">
-            
+            <span>- OR -</span>
+            <a href="/">Sign in with email</a>
         </div>
         <div className="captcha-cont">
 
         </div>
+        <br />
         <div className="btn-cont">
           <button className="otp-btn">Send OTP</button>
           <div class="cnfrm-inp-div">
             <span>You will recive otp on given number</span>
           </div>
         </div>
+        <br />
       </div>
     </div>
   ) : (
