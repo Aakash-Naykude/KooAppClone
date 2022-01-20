4f7e0ee556ab7b1c059f0da3c8c760da1de59cf7
vani-231 <vani630145@gmail.com>
2022-01-20 23:49:49 +0530

    about page

--- koo_app_clone/src/Components/Widgets/About.jsx
+++ koo_app_clone/src/Components/Widgets/About.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './About.css'
 
 function About() {
   return <div>
-      <h1>Hello hello</h1>
-      <Link to="/">
-          
-          Bach to Feed</Link>
+
+    <div id="back-to-feed" >
+      <Link to="/" >
+        <img className="icon" src="https://www.kooapp.com/assets/2d634360.svg" alt="3" />
+      </Link>
+
+      <Link to="/feed" >
+        <button className="btn"> 🢀 Back To Feed</button>
+      </Link>
+    </div>
+    <div className="about-us" >About Us</div>
+    <div className="matter">
+      <div>Koo is a micro-blog in Indian languages. We are here to help Indians express themselves in the easiest way possible with the objective of democratizing their voice. Share your thoughts in text, audio or video.</div>
+      <div>Some of the most prominent faces of India use Koo. You will also find millions of others from all walks of life. Koo is home to the Voices of India. Follow people you like, know what's on their mind and share your thoughts with India too.</div>
+      <div>Let's Koo together!</div>
+    </div>
+    <div className="all-queries" >
+      <Link to="/about">About</Link>
+    </div>
+    <div className="copy">Copyright Koo 2022 ©</div>
   </div>;
 }
+
 
 export default About;
