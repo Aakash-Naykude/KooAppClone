9681e405919c9572a782b1d8cc9ca5911caccb8b
Arnavi Gode <86406228+arnavigode@users.noreply.github.com>
2022-01-20 22:19:56 +0530

    pop-up updated

--- koo_app_clone/src/Components/LandingPage/LandingPage.jsx
+++ koo_app_clone/src/Components/LandingPage/LandingPage.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
@@ -7,8 +6,7 @@ import "./LandingPage.css";
 import LoginPopUp from "./LoginPopUp";
 
 const LandingPage = () => {
-
-    const [popup, setPopup] = useState(false);
+  const [popup, setPopup] = useState(false);
 
   return (
     <div>
@@ -52,13 +50,13 @@ const LandingPage = () => {
             <img
               src="https://img.icons8.com/small/96/000000/user-male-circle.png"
               alt="profile"
-              onClick={()=>{
-                  setPopup(true)
+              onClick={() => {
+                setPopup(true);
               }}
             />
           </div>
         </div>
-        <LoginPopUp trigger={popup} settrigger={setPopup} ><h3>Login popup</h3></LoginPopUp>
+        <LoginPopUp trigger={popup} settrigger={setPopup}></LoginPopUp>
       </div>
 
       <div className="pg-container">
@@ -98,6 +96,7 @@ const LandingPage = () => {
           <div></div>
         </div>
       </div>
+
       <div className="footer-container">
         <div className="footer-wrap">
           <div className="footer-list">
